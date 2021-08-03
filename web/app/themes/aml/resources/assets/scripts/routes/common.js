8bdae73692212dafc1e2810db545bfc92ddb2721
Richard Cantwell <hello@richardcantwell.ie>
2021-08-03 13:15:07 +0100

    Design change

--- web/app/themes/aml/resources/assets/scripts/routes/common.js
+++ web/app/themes/aml/resources/assets/scripts/routes/common.js
@@ -7,12 +7,12 @@ export default {
         $( 'a.idpal_btn_submit_user' ).on( 'click', function(e) {
             e.stopPropagation();
             var btn = $(this);
-            alert(window.aml_vars.ajax_url);
+            //alert(window.aml_vars.ajax_url);
             // console.log(aml_vars.ajax_url);
             // console.log($(this).data('id'));
             var user_id = btn.data('id'); console.log('user_id clicked .. ' + user_id); // alert(user_id);
             btn.text('One moment ...');
-            /*$.ajax({
+            $.ajax({
                 url: 'https://aml.tynandillon.ie/wp-admin/admin-ajax.php', // aml_vars.ajax_url,
                 type: 'POST',
                 data: {
@@ -27,7 +27,7 @@ export default {
                         location.reload();
                     }, 3000);
                 },
-            });*/
+            });
             return false;
         });
     },
